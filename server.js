429ffa7fb7089e3c919cca4036a84808ba21e604
willreed111185 <william.reed.11@gmail.com>
2018-01-06 23:26:39 -0500

    try again again

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-const keys = require('./config/keys');
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
