d4202d485fbd533a1af5174236df5b3a6c14af26
willreed111185 <william.reed.11@gmail.com>
2018-01-06 23:17:37 -0500

    try again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+//const routes = require("./routes");
 const app = express();
 const keys = require('./config/keys');
 const PORT = process.env.PORT || 3001;
