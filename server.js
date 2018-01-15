9693a3884d7ba57d856bf1f70dd137f7af12b6aa
willreed111185 <william.reed.11@gmail.com>
2018-01-14 22:10:20 -0500

    config updated

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const keys = require("./Config/keys")
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,7 +15,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  process.env.mongoDB_URL,
+  keys.mongodb.dbURI,
   {
     useMongoClient: true
   }
