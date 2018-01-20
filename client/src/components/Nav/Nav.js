5069ecf0975e1e4c69ce615ec4cb0674475157d5
willreed111185 <william.reed.11@gmail.com>
2018-01-20 01:03:26 -0500

    style

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,16 +3,10 @@ import React from "react";
 const Nav = () =>
   <nav className="navbar navbar-inverse navbar-top" style={{backgroundColor: 'white'}}>
     <div className="container-fluid">
-      <div className="navbar-inverss">
-        <a href="/" className="navbar-brand">
-          GoTED
-        </a>
-        <a href="/talks" className="navbar-brand">
-          Talks
-        </a>
-        <a href="/login" className="navbar-brand">
-          Admin
-        </a>
+      <div className="navbar-inverse" size="sm-12">
+        <a href="/" className="navbar-brand">GoTED</a>
+        <a href="/talks" className="navbar-brand">Talks</a>
+        <a href="/login" className="navbar-brand">Admin</a>
       </div>
     </div>
   </nav>;
