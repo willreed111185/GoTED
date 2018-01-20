2e6bacbb6652f30d307fe28095ed812f6f7cb82c
willreed111185 <william.reed.11@gmail.com>
2018-01-20 11:33:04 -0500

    style

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(routes);
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.mongoDB_URL||"mongodb://goTED:willreed@ds237947.mlab.com:37947/goted",
+mongoose.connect(process.env.mongoDB_URL,
   {
     useMongoClient: true
   }
